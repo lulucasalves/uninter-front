5459c500a0840d42dc0087237c65a0cc2f4a9f58
lulucasalves <lucas.alves.supus@outlook.com>
2022-09-02 00:12:42 -0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// Função de abrir nova página
+function openPage(link) {
+  window.open(link, '_blank')
+}
+
+// Função de acionar alert para avisar envio do formulário
+function sendMessage() {
+  alert('Mensagem enviada com sucesso!')
+}
